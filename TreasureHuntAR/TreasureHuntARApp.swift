132b6b389c13e1c62f554d3caca867da5184741f
Fabio <fabio.brambilla1998@gmail.com>
2022-04-19 09:16:41 +0200

    Initial Commit

---
+++ TreasureHuntAR/TreasureHuntARApp.swift
@@ -0,0 +1,17 @@
+//
+//  TreasureHuntARApp.swift
+//  TreasureHuntAR
+//
+//  Created by MacBook on 19/04/22.
+//
+
+import SwiftUI
+
+@main
+struct TreasureHuntARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
